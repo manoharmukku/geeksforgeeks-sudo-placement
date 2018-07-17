3f36cd6a0bc6fb58a86875d7787c82e024e3a292
Manohar Mukku <manoharmukku@gmail.com>
2018-07-17 14:13:23 +0530

    Rotating an array

---
+++ rotating-an-array.c
@@ -0,0 +1,35 @@
+/* 
+Author: Manohar Mukku
+Date: 17.07.2018
+Desc: GeeksforGeeks - Arrays - Rotating an Array
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int T;
+    scanf ("%d", &T);
+
+    while (T--){
+        int n;
+        scanf ("%d", &n);
+        
+        int* arr = (int*)malloc(n * sizeof(int));
+        
+        int i;
+        for (i = 0; i < n; i++) 
+            scanf ("%d", &arr[i]);
+
+        int d;
+        scanf ("%d", &d);
+
+        for (i = (d+n)%n; i < n && i != (d-1+n)%n; i = (i+1+n)%n)
+            printf("%d ", arr[i]);
+        printf("%d\n", arr[(d-1+n)%n]);
+
+        free(arr);
+    }
+
+    return 0;
+}
