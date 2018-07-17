aae4035be25257762bef17f61125843c12a5028c
Manohar Mukku <manoharmukku@gmail.com>
2018-07-17 14:13:42 +0530

    Rotate array by n elements

---
+++ rotate-array-by-n-elements.c
@@ -0,0 +1,32 @@
+/* 
+Author: Manohar Mukku
+Date: 17.07.2018
+Desc: GeeksforGeeks - Arrays - Rotate Array by n elements
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int T;
+    scanf ("%d", &T);
+
+    while (T--){
+        int n, d;
+        scanf ("%d %d", &n, &d);
+
+        int* arr = (int*)malloc(n * sizeof(int));
+        
+        int i;
+        for (i = 0; i < n; i++) 
+            scanf ("%d", &arr[i]);
+
+        for (i = (d+n)%n; i < n && i != (d-1+n)%n; i = (i+1+n)%n)
+            printf("%d ", arr[i]);
+        printf("%d\n", arr[(d-1+n)%n]);
+
+        free(arr);
+    }
+
+    return 0;
+}
