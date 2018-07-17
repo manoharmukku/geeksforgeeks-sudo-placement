7c63bc20b23bd872b1657385047e0c5bf73df443
Manohar Mukku <manoharmukku@gmail.com>
2018-07-17 14:13:04 +0530

    Reverse an array

---
+++ reverse-an-array.c
@@ -0,0 +1,32 @@
+/* 
+Author: Manohar Mukku
+Date: 17.07.2018
+Desc: GeeksforGeeks - Arrays - Leaders in an array
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int T;
+    scanf ("%d", &T);
+
+    while (T--) {
+        int n;
+        scanf ("%d", &n);
+
+        int* arr = (int*)malloc(n * sizeof(int));
+
+        int i;
+        for (i = 0; i < n; i++)
+            scanf("%d", &arr[i]);
+
+        for (i = n-1; i >= 0; i--) 
+            printf("%d ", arr[i]);
+        printf("\n");
+
+        free(arr);
+    }
+
+    return 0;
+}
